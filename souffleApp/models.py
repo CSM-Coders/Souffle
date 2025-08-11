86412eb4fe4ade19c001eb1eb5cd4fdc21a37d79
Calvarezv1705 <calvarezv1@eafit.edu.co>
2025-08-11 00:12:36 -0500

    colocacion de la descripcion, materiales, duracion, precio, ingredientes e informacion extra de los cursos

--- souffleApp/models.py
+++ souffleApp/models.py
@@ -5,5 +5,6 @@ from django.db import models
 class SouffleApp(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=250)
+    long_description = models.TextField(blank=True)
     image = models.ImageField(upload_to='souffleApp/images/')
     url = models.URLField(blank=True)
